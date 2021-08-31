add286d8a3d1295d13bdfd5a6ed683b4ed63b316
Abirbu <ariful.islam2874@gmail.com>
2021-08-31 19:46:56 +0600

    learned class

---
+++ class.js
@@ -0,0 +1,12 @@
+class friends {
+    constructor(fname, fphone){
+        this.Name = fname;
+        this.Address = 'Dumki, Patuakhali';
+        this.Phone = fphone;
+    }
+}
+
+const Basu = new friends('Basu', 01785569963);
+const Hridoy =new friends('Hridoy', 01799247210);
+
+console.log(Basu, Hridoy);
