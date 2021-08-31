68ca73110c295aabbce48f71d0338b15b8fb40e5
Abirbu <ariful.islam2874@gmail.com>
2021-08-31 12:21:24 +0600

    added template file

---
+++ template.js
@@ -0,0 +1,17 @@
+const fristName = 'Arifu';
+const lastName = 'islam';
+const fullName = fristName +" "+ lastName+" "+ "is a good boy";
+//we can use `` and make it easy
+const fullName2 = `${fristName} ${lastName} is a good boy`;
+console.log(fullName);
+console.log(fullName2);
+
+//to write in multiline
+const multiLine = "My name is Ariful Islam\n"
++ "I am a good boy."
+console.log(multiLine);
+//you canmake it easy by using ``
+
+const multiLine2 = `my name is Ariful ${lastName}
+i am a good boy `;
+console.log(multiLine2);
